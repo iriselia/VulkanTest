8ba865e2f6e7586f41167cd5d54c789276d07ce5
Frank Park <y.piao.us@ieee.org>
2016-05-31 19:29:38 -0700

    Initial Commit

---
+++ Source/RendererTest1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <fstream>
+#include <future>
+#include <regex>
+
+int main()
+{
+	while(1)
+	{
+	}
+	return 0;
+}
